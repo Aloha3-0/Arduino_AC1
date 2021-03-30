513414921e6d03b1968f05bc59a40550b3ae3f3d
sophiapuig <78865048+sophiapuig@users.noreply.github.com>
2021-03-30 20:55:13 -0300

    Declarar botão 2

--- AC1.ino
+++ AC1.ino
@@ -6,7 +6,9 @@ const int azul = 7;
 bool estadoLedVermelho = false;
 
 const int botao1 = 2;
+const int botao2 = 3;
 unsigned long lastDebounceTime1 = 0;
+unsigned long lastDebounceTime2 = 0;
 const int botaoDelay = 100;
 
 void setup()
